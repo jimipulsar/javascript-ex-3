97bce33fe0eb3302c37fdef5422395f203e6a38b
jimipulsar <jimipulsar@gmail.com>
2018-11-17 17:39:49 +0100

    Esercizio corretto in ordine alfabetico

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 var nome_access = prompt("Inserisci il tuo cognome");
-
-var array = [ "Rossi", "Colapinto", "Castaldini", "Papagni" , "Ricci", "Desantis", "Caldarola" ];
+var separatore = "<br>";
+var array = [ "Rossi" , "Colapinto" , "Castaldini" , "Papagni" , "Ricci", "Desantis" , "Caldarola"  ];
 
 var cognome =  "Caldarola";
+
+var alfa = new Array("<li>");
 
 var contatore = 0;
 var trovato = false;
 
 var lista = document.getElementById('lista');
+var arraySorted = array.slice(); //faccio una copia dell'array per non perdere la posizione iniziale
+arraySorted.sort(); // metto in ordine alfabetico i risultati
 
-while (!trovato && contatore < array.length) {
+
+while (!trovato && contatore < arraySorted.length) {
   if (array[contatore] == nome_access) {
     trovato = true;
   }
-  console.log(array[contatore]);
+    console.log(array[contatore]);
   contatore++;
 }
 
 if (trovato == true) {
-  document.writeln("<h1>Congratulazioni! Sei presente nella lista.</h1>"  + "Il tuo cognome si trova in " + contatore + "° posizione" + "<br>" + "<h3>Ecco la lista dei partecipanti: </h3>" + array ) ;
+  document.writeln("<h1>Congratulazioni! Sei presente nella lista.</h1>"  + "Il tuo cognome si trova in " + contatore + "° posizione" + "<br>" + "<h3>Ecco la lista dei partecipanti: </h3>") ;
+  document.write(array.sort());
   console.log("Accesso consentito.");
+
 }
 
 else {
   document.writeln("<h1>Non sei presente nella lista e non potrai visualizzarla.</h1>" ) ;
   console.log("Accesso negato.");
+
 }
